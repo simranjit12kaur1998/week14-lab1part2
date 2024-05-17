4813664123802bbd69ee2740da5f80afe7c7b576
simran <Simranjit.Kaur2@metropolia.fi>
2024-05-18 00:15:25 +0530

    lab1part2 finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+// App.js
+function Hello() {
+  return <p>Hello, React!</p>;
+}
+// App.js
+function Bye() {
+  return <p>Goodbye, React!</p>;
+}
+// App.js
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hello />
+      <Bye />
     </div>
   );
 }
